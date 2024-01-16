7154f89889135620e194bb52731e542b23b0e312
Hypexgene <ashtonmuiru@gamil.com>
2024-01-16 14:01:15 -0800

    ashtontask

--- models/base_model.py
+++ models/base_model.py
@@ -3,9 +3,9 @@
 
 import uuid
 from datetime import datetime
-from . import storage as data_storage
+import models
 
-class UniqueModel:
+class BaseModel:
     """This class serves as the foundation for all other classes in our unique project"""
 
     def __init__(self, *args, **kwargs):
@@ -15,32 +15,31 @@ class UniqueModel:
             - *args: Additional arguments (not a must)
             - **kwargs: Keyword arguments in a dictionary form (not a must)
         """
-        unique_storage = data_storage.UniqueStorage()
         if kwargs and len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
                 else:
-                    self.__dict__[key] = value
+                    setattr(self, key, value)
         else:
-            self.unique_id = str(uuid.uuid4())  # A unique twist to the variable name
-            self.creation_time = datetime.now()  # Renaming for uniqueness
-            self.last_updated = datetime.now()  # Renaming for uniqueness
-            unique_storage.new(self)
+            self.id = str(uuid.uuid4()) 
+            self.created_at = datetime.now() 
+            self.updated_at = datetime.now()  
+            models.storage.new(self)
 
     def __str__(self):
         """Return a distinct string representation for our unique project"""
-        return "[{}] ({}) {}".format(type(self).__name__, self.unique_id, self.__dict__)
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
-    def save_instance(self):
+    def save(self):
         """Update the public instance attribute last_updated uniquely"""
-        self.last_updated = datetime.now()
-        unique_storage.save()
+        self.updated_at = datetime.now()
+        models.storage.save()
 
-    def to_unique_dict(self):
-        """Return a dictionary containing all keys/values of __dict__ with a unique twist"""
-        unique_dict = self.__dict__.copy()
-        unique_dict["__unique_class__"] = type(self).__name__  # Adding a unique class key
-        unique_dict["creation_time"] = unique_dict["creation_time"].isoformat()  # Unique variable name
-        unique_dict["last_updated"] = unique_dict["last_updated"].isoformat()  # Unique variable name
-        return unique_dict
+    def to_dict(self):
+        """Return a dictionary representation of the object."""
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = type(self).__name__
+        obj_dict["created_at"] = obj_dict["created_at"].isoformat()
+        obj_dict["updated_at"] = obj_dict["updated_at"].isoformat()
+        return obj_dict
